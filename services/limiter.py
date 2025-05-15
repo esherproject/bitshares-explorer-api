16ef7598bac43996b1ffb9fb553b5757d3de88f9
Altecoin Crypto <87372685+altecoin-altc@users.noreply.github.com>
2025-05-15 16:11:25 +0800

    Update limiter.py

--- services/limiter.py
+++ services/limiter.py
@@ -3,10 +3,9 @@ from flask_limiter.util import get_remote_address
 
 
 def init(app):
-
     limiter = Limiter(
-        app,
         key_func=get_remote_address,
         default_limits=[]
     )
+    limiter.init_app(app)
     return limiter
